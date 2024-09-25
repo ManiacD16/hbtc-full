b5537b7cd2a9bd0d18cd87d8dd4e0f4079362c5b
ManiacD16 <msd161918@gmail.com>
2024-09-25 12:16:40 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Features from "./Components/Features";
